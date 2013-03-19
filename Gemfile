04c3b243ed967e2caaebdcc895c1ba601f656b23
Lawson Kurtz <lawson.kurtz@viget.com>
2013-03-19 08:43:58 -0400

    Base Rails

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+gem 'jquery-rails'
+gem 'simple_form'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'pry'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'simplecov'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corneroffice::Application.config.secret_token = '5024be39d61f14b4852754450f411339a0118cf4b3d41ea08b7981e3a7d18be4cbae4b3b0be6bf1e17f32dd72927629feaa47290cd0a42a3fe5410223ea01430'
